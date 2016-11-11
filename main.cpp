1e89949d04bf24d58408b2d499adb4ffbdf4343e
Quentin <Quentin@DESKTOP-QR3C5CR>
2016-11-11 23:38:56 +0100

    Ajoutez des fichiers projet.

---
+++ main.cpp
@@ -0,0 +1,27 @@
+﻿#include <SFML/Graphics.hpp>
+
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+	sf::CircleShape shape(100);
+	shape.setFillColor(sf::Color::Green);
+	int pos_x = 0;
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+			if (event.type == sf::Event::key.LEFT)
+				sf::CircleShape shape(pos_x++);
+		}
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
+
+	return 0;
+}
